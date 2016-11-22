50ece2b0e068ee5c7b8fa131b491e8ee4834bf49
lcx <lcxlinux@163.com>
2016-11-23 00:53:22 +0800

    最新

--- lstate.h
+++ lstate.h
@@ -3,19 +3,37 @@
 
 #include "xlua.h"
 #include "lobject.h"
+#include "ldo.h"
+
+
+typedef struct CallInfo{
+	StkId func;
+	StkId top;
+}CallInfo;
 
 typedef struct{
 	struct lua_State *twups;
 	struct lua_State *mainthread;
 	unsigned int seed;
+
+	struct Table *mt[LUA_NUMTAGS];
 }global_State;
 
-typedef struct lua_State{
+struct lua_State{
 	CommonHeader;
-	global_State *l_G;
-}lua_State;
 
-#define G(L) (l->l_G)
+	StkId stack;
+	StkId top;
+	StkId stack_last;
+
+	unsigned short nci;
+	CallInfo *ci;
+	CallInfo *base_ci;
+
+	global_State *l_G;
+};
+
+#define G(L) (L->l_G)
 
 extern lua_State *lua_newstate(lua_Alloc f,void *ud);
 
