520b1a6cae03005fdcf8fce046ddd201d992e045
lcx <lcxlinux@163.com>
2016-11-24 02:39:45 +0800

    更新xlua.h

--- xlua.h
+++ xlua.h
@@ -1,11 +1,19 @@
 #ifndef xlua_h
 #define xlua_h
 
-#define LUA_TTHREAD 8
+#include<stdio.h>
+
+#define LUA_OK	0
+
+#define LUA_TTHREAD	8
+
+#define LUA_NUMTAGS	9
 
 typedef unsigned char lu_byte;
 
 #define cast(t,exp) ((t)(exp))
+
+typedef struct lua_State lua_State;
 
 typedef void* (*lua_Alloc)(void *ud,void *ptr,size_t osize,size_t nsize);
 
