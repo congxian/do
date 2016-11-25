4cf08cd82b0b3a0b25bf8f640970a1eab790f342
lcx <lcxlinux@163.com>
2016-11-25 23:52:17 +0800

    lstring.c

---
+++ lstring.c
@@ -0,0 +1,7 @@
+#include "lstate.h"
+#include "lstring.h"
+
+
+void luaS_init(lua_State *L){
+	printf("string init\n");
+}
---
+++ lstring.h
@@ -0,0 +1,7 @@
+#ifndef lstring_h
+#define lstring_h
+
+
+extern void luaS_init(lua_State *L);
+
+#endif
